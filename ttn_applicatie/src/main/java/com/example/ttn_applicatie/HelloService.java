3cf1f30f644a7a0917c3da7e8d7039be03da492e
Smolders Rik,R <riksmol25@gmail.com>
2023-10-10 15:38:31 +0200

    Frontend

--- ttn_applicatie/src/main/java/com/example/ttn_applicatie/HelloService.java
+++ ttn_applicatie/src/main/java/com/example/ttn_applicatie/HelloService.java
@@ -1,14 +1,14 @@
 package com.example.ttn_applicatie;
 
-import com.mongodb.*;
-import com.mongodb.client.*;
-import com.mongodb.connection.ClusterDescription;
-import org.bson.Document;
-import org.bson.conversions.Bson;
+import org.json.*;
+import org.bson.BsonArray;
+import org.bson.BsonDocument;
+import org.bson.BsonValue;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -17,8 +17,28 @@ public class HelloService {
     @RequestMapping(
             value="/",
             method= RequestMethod.GET)
-    public String hello(){
+    public String hello() throws JSONException {
+        BsonArray array = TtnApplicatieApplication.DBConn();
 
+        if (array != null){
+            String arrayString = "";
+
+            // Make a json object from all bsonvalues
+            for (BsonValue value:
+                 array.getValues()) {
+                BsonDocument doc = new BsonDocument();
+                doc.put("Value", value);
+                // JsonWriterSettings.Builder settingsbuilder = JsonWriterSettings.builder().indent(true).outputMode(JsonMode.EXTENDED);
+                JSONObject jsonObject = new JSONObject(doc.toJson());
+
+                String temp = String.valueOf(jsonObject.getJSONObject("Value").getJSONObject("sensor_readings").getDouble("Temperatuur"));
+                String pH = String.valueOf(jsonObject.getJSONObject("Value").getJSONObject("sensor_readings").getDouble("pH"));
+                String dataID = String.valueOf(jsonObject.getJSONObject("Value").getString("_id"));
+
+                arrayString += " | " + dataID + " (Temperatuur: " + temp + ", pH-waarde: " + pH + ") | \n \n";
+            }
+            return arrayString;
+        }
 
         return "hello";
     }
--- ttn_applicatie/src/main/java/com/example/ttn_applicatie/TtnApplicatieApplication.java
+++ ttn_applicatie/src/main/java/com/example/ttn_applicatie/TtnApplicatieApplication.java
@@ -3,6 +3,7 @@ package com.example.ttn_applicatie;
 import com.mongodb.*;
 import com.mongodb.client.*;
 import com.mongodb.client.model.Filters;
+import org.bson.BsonArray;
 import org.bson.Document;
 import org.bson.conversions.Bson;
 import org.bson.json.JsonMode;
@@ -18,11 +19,9 @@ import java.util.Set;
 public class TtnApplicatieApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TtnApplicatieApplication.class, args);
-
-		DBConn();
 	}
 
-	public static void DBConn(){
+	public static BsonArray DBConn(){
 		String connectionString = "mongodb+srv://202324_AD:VdSPg3g77xFGSTmb@cluster0.hpcak9m.mongodb.net/?retryWrites=true&w=majority";
 		ServerApi serverApi = ServerApi.builder()
 				.version(ServerApiVersion.V1)
@@ -41,12 +40,14 @@ public class TtnApplicatieApplication {
 				System.out.println("Number of collections in db: " + collection.countDocuments());
 
 				JsonWriterSettings jsonSettings = JsonWriterSettings.builder().outputMode(JsonMode.RELAXED).build();
-				Bson filter = Filters.and(Filters.gt("SensorData", 1), Filters.lt("SensorData", 100));
-				collection.find().forEach(doc -> System.out.println(doc.toJson()));
+				BsonArray bsonarray = new BsonArray();
+				collection.find().limit(10).forEach(doc -> bsonarray.add(doc.toBsonDocument()));
 
+				return bsonarray;
 			} catch (MongoException e) {
 				e.printStackTrace();
 			}
+			return null;
 		}
 	}
 
