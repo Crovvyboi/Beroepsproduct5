7505e36fc3c8bc64cfb977dcb8f8fc050152481c
Smolders Rik,R <riksmol25@gmail.com>
2023-10-10 11:31:32 +0200

    Init springboot setup

---
+++ ttn_applicatie/src/main/java/com/example/ttn_applicatie/TtnApplicatieApplication.java
@@ -0,0 +1,13 @@
+package com.example.ttn_applicatie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtnApplicatieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtnApplicatieApplication.class, args);
+	}
+
+}
---
+++ ttn_applicatie/src/test/java/com/example/ttn_applicatie/TtnApplicatieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ttn_applicatie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtnApplicatieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
