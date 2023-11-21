cfd8470a4ecda72468c55049f142048ae44955c4
Smolders Rik,R <riksmol25@gmail.com>
2023-11-21 12:27:39 +0100

    Added endpoints
    batches, date, ph range and temp range

--- ttn_backend/src/main/java/com/example/demo/SensorDataHandler.java
+++ ttn_backend/src/main/java/com/example/demo/SensorDataHandler.java
@@ -17,9 +17,10 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.boot.json.JsonParser;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 
 
 @RestController
@@ -30,7 +31,56 @@ public class SensorDataHandler {
             value="/",
             method= RequestMethod.GET,
             produces = "application/json")
-    public ResponseEntity<String> GetAll(){
+    public ResponseEntity<String> GetAll(@RequestParam(required = false) Integer batchsize, @RequestParam(required = false) Integer batchpos){
+        ServerApi serverApi = ServerApi.builder()
+                .version(ServerApiVersion.V1)
+                .build();
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(connectionString))
+                .serverApi(serverApi)
+                .build();
+        // Create a new client and connect to the server
+        try (MongoClient mongoClient = MongoClients.create(settings)) {
+            try {
+                // Send a ping to confirm a successful connection
+                MongoDatabase database = mongoClient.getDatabase("202324_SensorProj");
+
+                MongoCollection<Document> collection = database.getCollection("SensorData");
+                System.out.println("Number of collections in db: " + collection.countDocuments());
+
+                BsonArray bsonarray = new BsonArray();
+                if (batchsize == null || batchpos == null){
+                    collection.find().forEach(doc -> bsonarray.add(doc.toBsonDocument()));
+                }
+                else{
+                    collection.find().skip(batchpos * batchsize).limit(batchsize).forEach(doc -> bsonarray.add(doc.toBsonDocument()));
+                }
+
+                JSONArray json = JSONArrayBuilder(bsonarray);
+
+                if (json.length() < 1){
+                    return ResponseEntity.noContent().build();
+                }
+                else {
+                    JSONObject object = new JSONObject();
+                    object.put("Object", json);
+                    return ResponseEntity.ok(object.toString());
+                }
+            } catch (MongoException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                throw new RuntimeException(e);
+            }
+            return null;
+        }
+    }
+
+    // Per date / month
+    @RequestMapping(
+            value="/bydate/",
+            method= RequestMethod.GET,
+            produces = "application/json")
+    public ResponseEntity<String> GetByDate(@RequestParam int day, @RequestParam int month, @RequestParam int year){
         ServerApi serverApi = ServerApi.builder()
                 .version(ServerApiVersion.V1)
                 .build();
@@ -50,10 +100,123 @@ public class SensorDataHandler {
                 BsonArray bsonarray = new BsonArray();
                 collection.find().forEach(doc -> bsonarray.add(doc.toBsonDocument()));
 
-                JSONArray json = JSONArrayBuilder(bsonarray);
+                JSONArray json = JSONArrayBuilder(bsonarray, day, month, year);
 
                 if (json.length() < 1){
-                    return ResponseEntity.notFound().build();
+                    return ResponseEntity.noContent().build();
+                }
+                else {
+                    JSONObject object = new JSONObject();
+                    object.put("Object", json);
+                    return ResponseEntity.ok(object.toString());
+                }
+            } catch (MongoException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                throw new RuntimeException(e);
+            }
+            return null;
+        }
+    }
+
+    // Check if value is outside wanted perimiter
+    @RequestMapping(
+            value="/phrange/",
+            method= RequestMethod.GET,
+            produces = "application/json")
+    public ResponseEntity<String> GetPHRange(@RequestParam(required = false) Double phmin, @RequestParam(required = false) Double phmax){
+        ServerApi serverApi = ServerApi.builder()
+                .version(ServerApiVersion.V1)
+                .build();
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(connectionString))
+                .serverApi(serverApi)
+                .build();
+        // Create a new client and connect to the server
+        try (MongoClient mongoClient = MongoClients.create(settings)) {
+            try {
+                // Send a ping to confirm a successful connection
+                MongoDatabase database = mongoClient.getDatabase("202324_SensorProj");
+
+                MongoCollection<Document> collection = database.getCollection("SensorData");
+                System.out.println("Number of collections in db: " + collection.countDocuments());
+
+                BsonArray bsonarray = new BsonArray();
+                collection.find().forEach(doc -> bsonarray.add(doc.toBsonDocument()));
+
+                JSONArray json;
+
+                if (phmax != null && phmin != null){
+                    json = JSONArrayBuilderPH(bsonarray, phmax, phmin);
+                }
+                else if (phmax == null && phmin != null){
+                    json = JSONArrayBuilderPH(bsonarray, 10.0, phmin);
+                }
+                else if (phmax != null && phmin == null){
+                    json = JSONArrayBuilderPH(bsonarray, phmax, 0.0);
+                }
+                else {
+                    json = JSONArrayBuilder(bsonarray);
+                }
+
+                if (json.length() < 1){
+                    return ResponseEntity.noContent().build();
+                }
+                else {
+                    JSONObject object = new JSONObject();
+                    object.put("Object", json);
+                    return ResponseEntity.ok(object.toString());
+                }
+            } catch (MongoException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                throw new RuntimeException(e);
+            }
+            return null;
+        }
+    }
+
+    @RequestMapping(
+            value="/temprange/",
+            method= RequestMethod.GET,
+            produces = "application/json")
+    public ResponseEntity<String> GetTempRange(@RequestParam(required = false) Double tempmin, @RequestParam(required = false) Double tempmax){
+        ServerApi serverApi = ServerApi.builder()
+                .version(ServerApiVersion.V1)
+                .build();
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(connectionString))
+                .serverApi(serverApi)
+                .build();
+        // Create a new client and connect to the server
+        try (MongoClient mongoClient = MongoClients.create(settings)) {
+            try {
+                // Send a ping to confirm a successful connection
+                MongoDatabase database = mongoClient.getDatabase("202324_SensorProj");
+
+                MongoCollection<Document> collection = database.getCollection("SensorData");
+                System.out.println("Number of collections in db: " + collection.countDocuments());
+
+                BsonArray bsonarray = new BsonArray();
+                collection.find().forEach(doc -> bsonarray.add(doc.toBsonDocument()));
+
+                JSONArray json;
+
+                if (tempmax != null && tempmin != null){
+                    json = JSONArrayBuilderTemp(bsonarray, tempmax, tempmin);
+                }
+                else if (tempmax == null && tempmin != null){
+                    json = JSONArrayBuilderTemp(bsonarray, null, tempmin);
+                }
+                else if (tempmax != null && tempmin == null){
+                    json = JSONArrayBuilderTemp(bsonarray, tempmax, null);
+                }
+                else {
+                    json = JSONArrayBuilder(bsonarray);
+                }
+
+                if (json.length() < 1){
+                    return ResponseEntity.noContent().build();
                 }
                 else {
                     JSONObject object = new JSONObject();
@@ -76,6 +239,72 @@ public class SensorDataHandler {
                 bsonArray.getValues()) {
             JSONObject obj = new JSONObject(value.asDocument().toJson());
             jsonObject.put(obj);
+        }
+
+        return jsonObject;
+    }
+
+    public JSONArray JSONArrayBuilder(BsonArray bsonArray, int day, int month, int year) throws JSONException {
+        JSONArray jsonObject = new JSONArray();
+
+        for (BsonValue value:
+                bsonArray.getValues()) {
+            JSONObject obj = new JSONObject(value.asDocument().toJson());
+
+            String dateString = obj.getJSONObject("received_at").getString("$date");
+            ZonedDateTime dateTimeZoned = ZonedDateTime.parse(dateString);
+            LocalDateTime dateTime = dateTimeZoned.toLocalDateTime();
+
+            if (dateTime.getDayOfMonth() == day && dateTime.getMonthValue() == month && dateTime.getYear() == year){
+                jsonObject.put(obj);
+            }
+        }
+
+        return jsonObject;
+    }
+
+    public JSONArray JSONArrayBuilderPH(BsonArray bsonArray, Double phMax, Double phMin) throws JSONException {
+        JSONArray jsonObject = new JSONArray();
+
+        for (BsonValue value:
+                bsonArray.getValues()) {
+            JSONObject obj = new JSONObject(value.asDocument().toJson());
+
+            Double pH = Double.valueOf(String.valueOf(obj.getJSONObject("sensor_readings").getDouble("pH")));
+
+            if (phMin < pH && pH < phMax){
+                jsonObject.put(obj);
+            }
+
+        }
+
+        return jsonObject;
+    }
+
+    public JSONArray JSONArrayBuilderTemp(BsonArray bsonArray, Double tempMax, Double tempMin) throws JSONException {
+        JSONArray jsonObject = new JSONArray();
+
+        for (BsonValue value:
+                bsonArray.getValues()) {
+            JSONObject obj = new JSONObject(value.asDocument().toJson());
+
+            Double temp = Double.valueOf(String.valueOf(obj.getJSONObject("sensor_readings").getDouble("Temperatuur")));
+
+            if (tempMax != null && tempMin != null){
+                if (tempMin < temp && temp < tempMax){
+                    jsonObject.put(obj);
+                }
+            }
+            else if (tempMax == null && tempMin != null){
+                if (tempMin < temp){
+                    jsonObject.put(obj);
+                }
+            }
+            else if (tempMax != null && tempMin == null){
+                if (temp < tempMax){
+                    jsonObject.put(obj);
+                }
+            }
         }
 
         return jsonObject;
