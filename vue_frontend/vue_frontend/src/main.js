edc163392df36d53e62b237569e8ede0e626b156
Crovvyboi <81975137+Crovvyboi@users.noreply.github.com>
2024-01-20 15:31:30 +0100

    Frontend

--- vue_frontend/vue_frontend/src/main.js
+++ vue_frontend/vue_frontend/src/main.js
@@ -1,38 +1,23 @@
-import './assets/main.css'
+// main.js
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';  // Importeer de router
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+import '@mdi/font/css/materialdesignicons.css';
 
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-import { createAuth0 } from '@auth0/auth0-vue';
-import { firebaseApp } from './firebase';
-
-// const vuetify = createVuetify({
-//   components,
-//   directives,
-// })
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+  }
+});
 
 const app = createApp(App);
-app.use(
-  createAuth0({
-    domain: "dev-dvcmi3f0j8fjslxr.us.auth0.com",
-    clientId: "EOEZz7tJt1hEt1DQuEECGFj5zBhXhvIj",
-    authorizationParams: {
-      redirect_uri: window.location.origin
-    }
-  })
-);
-
-app.use(
-  createVuetify({
-    components,
-    directives,
-  })
-);
-
-
+app.use(vuetify);
+app.use(router);  // Voeg de router toe aan de app
 app.mount('#app');
-// createApp(App).use(vuetify).mount('#app')
---
+++ vue_frontend/vue_frontend/src/views/Home.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="center-container">
+      <!-- Voeg hier eventuele inhoud toe voor Historie -->
+      <router-link to="/actuele-data" class="clickable-wrapper" style="width: 10%;">
+        <div class="center-image-container">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/Circle-icons-water.svg/2048px-Circle-icons-water.svg.png" alt="Water icoon" class="center-image" />
+        </div>
+      </router-link>
+    </div>
+  </template>
+  
+  <style>
+  .center-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 90vh; /* Volledige hoogte van het scherm */
+  }
+  
+  .center-image-container {
+    text-align: center;
+  }
+  
+  .center-image {
+    max-width: 100%; /* Zorg ervoor dat de afbeelding niet breder is dan de container */
+    max-height: auto; /* Zorg ervoor dat de afbeelding niet hoger is dan de container */
+  }
+  
+  .clickable-wrapper {
+    cursor: pointer;
+    text-decoration: none; /* Verwijder onderstreepte stijl voor router-link */
+    transition: width 0.3s; /* Voeg een overgang toe voor een vloeiende animatie */
+  }
+  
+  .clickable-wrapper:hover {
+    width: 10%; /* Verander de breedte wanneer de gebruiker er overheen zweeft */
+  }
+  </style>
+  
